47ef5dceac46fff3a57958992c1b7e8437ef4e36
Govi451 <113189162+Govi451@users.noreply.github.com>
2023-07-01 13:25:35 +0530

    Add files via upload

---
+++ RequireAssertRevert.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract DivideByZero {
+    function divide(uint256 numerator, uint256 denominator) public pure returns (uint256) {
+        require(denominator != 0, "Division by zero is not allowed");
+        return numerator / denominator;
+    }
+    
+    function divideWithAssert(uint256 numerator, uint256 denominator) public pure returns (uint256) {
+        assert(denominator != 0);
+        return numerator / denominator;
+    }
+    
+    function divideWithRevert(uint256 numerator, uint256 denominator) public pure returns (uint256) {
+        if (denominator == 0) {
+            revert("Division by zero is not allowed");
+        }
+        return numerator / denominator;
+    }
+}
